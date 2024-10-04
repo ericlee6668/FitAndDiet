9c829b261fb98143e47ff875914291999b3607d8
clark <ericlee68388@gmail.com>
2024-10-04 16:53:31 +0700

    去除appsflyer_sdk

--- fooddiary/lib/main.dart
+++ fooddiary/lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:appsflyer_sdk/appsflyer_sdk.dart';
 import 'package:fit_track/common/global/constants.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
